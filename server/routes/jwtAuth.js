0db034c39b8989352ecc56051ca8a3f891180c31
Andrew <andtran@umass.edu>
2022-02-24 23:41:49 -0500

    package sql commands in a transaction

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -7,25 +7,25 @@ const authorization = require("../middleware/authorization");
 router.post("/register", async (req, res) => {
     try{
         const {id, fname, lname, pass, admin} = req.body;
-
-        const employee = await pool.query("SELECT * FROM employee WHERE empl_id = $1", [id]);
-
+        
+        const employee = await pool.query("SELECT * FROM employee WHERE id = $1", [id]);
 
         if (employee.rows.length !== 0) {
-            return res.status(401).send("Employee already in database");
+            return res.status(401).json("Employee already in database");
         }
 
         const saltRound = 10;
         const salt = await bcrypt.genSalt(saltRound);
         const hashed_pass = await bcrypt.hash(pass, salt);
         
+        await pool.query("BEGIN TRANSACTION");
         const new_employee = await pool.query("INSERT INTO employee VALUES($1, $2, $3, $4, $5) RETURNING *", [id, fname, lname, hashed_pass, admin]);
-        
         let new_user = "CREATE USER " + fname + "_" + lname + "_" + id.toString() + " WITH PASSWORD '" + pass + "'";
         admin ? new_user += "IN role admin" : new_user += "IN role non_admin";
         await pool.query(new_user);
+        await pool.query("COMMIT");
 
-        const token = jwtGenerator(new_employee.rows[0].empl_id);
+        const token = jwtGenerator(new_employee.rows[0].id);
 
         return res.json({token});
 
@@ -40,10 +40,10 @@ router.post("/login", async (req, res) => {
     try {
         const {id, pass} = req.body;
 
-        const employee = await pool.query("SELECt * FROM employee WHERE empl_id = $1", [id]);
+        const employee = await pool.query("SELECt * FROM employee WHERE id = $1", [id]);
 
         if (employee.rows.length === 0) {
-            return res.status(401).send("No matching id/password combination");
+            return res.status(401).json("No matching id/password combination");
         }
         
         const validPassword = await bcrypt.compare(pass, employee.rows[0].pass);
@@ -52,7 +52,7 @@ router.post("/login", async (req, res) => {
             return res.status(401).json("No matching id/password combination");
         }
 
-        const token = jwtGenerator(employee.rows[0].empl_id);
+        const token = jwtGenerator(employee.rows[0].id);
 
         res.json({token});
 
