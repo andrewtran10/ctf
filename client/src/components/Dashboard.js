94d9a7851e92d80201e9317a33c34a93c9f0bdbb
Andrew <andtran@umass.edu>
2022-02-26 17:57:13 -0500

    Dashboard overhaul

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import { React, Fragment } from 'react';
 import { useState, useEffect } from 'react';
 
-import { Grid } from '@mui/material';
+import { Grid, InputLabel, MenuItem, TextField } from '@mui/material';
 import { Select, Button } from '@mui/material';
 import { Typography } from '@mui/material';
+import { FormControl, Input } from '@mui/material';
 
 import { toast } from 'react-toastify';
+import Register from './Register';
 
 
 
@@ -14,25 +16,29 @@ const defaultVals = {
     fname: "",
     lname: "",
     pass: "",
-    admin: false
+    admin: false,
+    tables: []
 };
 
 
 const Dashboard = ({setAuth}) =>  {
     const [employeeData, setEmployeeData] = useState(defaultVals);
+    const [table, setTable] = useState("");
+    const [file, setFile] = useState(null);
 
-    const getEmployeeProfile =  async () => {
+    const getEmployeeData =  async () => {
         try {
-            const res = await fetch(
+            await fetch(
                 "http://localhost:5000/dashboard", 
                 {
                     method: "GET",
                     headers: { token: localStorage.token }
-                });
-
-            const parsedRes = await res.json();
-            setEmployeeData(parsedRes);
-
+                }).then(
+                    res => res.json()
+                ).then(
+                    res => setEmployeeData(res)
+                );
+                
         } catch (error) {
             console.error(error.message);
         }
@@ -47,17 +53,46 @@ const Dashboard = ({setAuth}) =>  {
         } catch (error) {
             console.error(error.message);
         }
+    };
+
+    const tableChange = (event) => {
+        setTable(event.target.value);
+    };
+
+    const handleTransform = async () => {
+        if (table === "") {
+            toast.error("No table selected");
+            return;
+        }
+        try {
+            await fetch("http://localhost:5000/dashboard/transform",
+                {
+                    method: "GET",
+                    headers: {token: localStorage.token, id: employeeData.id, table: table}
+                }
+            )
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const fileChange = (event) => {
+        setFile(event.target.files[0]);
     }
 
+    const handleUpload = async () => {
+        console.log(file);
+    };
+
     useEffect(() => {
-        getEmployeeProfile();
+        getEmployeeData();
     }, []);
 
     return (
         <Fragment>
             <Grid
                 container 
-                spacing={2}
+                spacing={5}
                 direction="column"
                 justifyContent="center"
                 alignItems="center"    
@@ -65,21 +100,93 @@ const Dashboard = ({setAuth}) =>  {
                 <Grid 
                     item
                     container 
-                    spacing={2}
                     direction="row"
                     justifyContent="center"
                     alignItems="center"    
                 >
-                    <Grid item xs={10}>
+                    <Grid item xs={11}>
                         <Typography variant='h2' component='div' align='center'>
                             {employeeData.fname} {employeeData.lname} Dashboard    
                         </Typography>
                     </Grid>
-                    <Grid item xs={2}>
+                    <Grid item xs={1}>
                         <Button onClick={e => logout(e)} color="primary" variant="contained"> Logout </Button>
                     </Grid>
                 </Grid>
 
+
+                <Grid 
+                    item
+                    container
+                    spacing={2}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Typography variant='h5'>Use pickle file to upload to database</Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <label htmlFor='pkl-upload'>
+                            <input
+                                id='pkl-upload'
+                                name='pkl-uoload'
+                                type='file'
+                                onChange={fileChange}
+                            />
+                        </label>
+                        <Button
+                                variant="contained"
+                                color='primary'
+                                component='span'
+                                onClick={handleUpload}
+                        > 
+                            Upload 
+                        </Button>
+                    </Grid>
+                </Grid>
+
+            
+                <Grid 
+                    item
+                    container
+                    spacing={2}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Typography variant='h5'> Select table for transform </Typography>
+                    </Grid>
+                    <Grid item>
+                        <FormControl style={{minWidth: 150}}>
+                            <InputLabel id='table-selector-label'> Tables </InputLabel>
+                            <Select
+                                labelId='table-selector-label'
+                                id='table-selector'
+                                value={table}
+                                label='Table'
+                                onChange={tableChange}
+                            >
+                                {employeeData.tables.map((table, index) => {
+                                    return <MenuItem key={index} value={table}> {table} </MenuItem>
+                                })}
+
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item>
+                        <Button 
+                            variant='contained' 
+                            color='primary' 
+                            onClick={handleTransform}
+                        > 
+                            Transform 
+                        </Button>
+                    </Grid>
+                </Grid>
+                { employeeData.admin && <Grid item> <Register/> </Grid> }
             </Grid>
         </Fragment>
     );
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,13 +5,26 @@ const authorization = require("../middleware/authorization");
 router.get("/", authorization, async (req,res) => {
     try {
         const employee = await pool.query("SELECT id, fname, lname, admin FROM employee where id = $1", [req.id]);
+        const tables = await pool.query("SELECT tablename FROM pg_tables WHERE tableowner = $1", ["u"+req.id])
 
-        res.json(employee.rows[0]);
+        res_data = employee.rows[0];
+
+        (tables.rows.length > 0) ? res_data.tables = [tables.rows[0].tablename] : res_data.tables = [];
+
+        res.json(res_data);
         
     } catch (error) {
         console.error(error.message);
         res.status(500).json("Sever error");
     }
+});
+
+router.get("/upload", authorization, (req,res) => {
+
+});
+
+router.get("/transform", authorization, (req, res) => {
+
 });
 
 
