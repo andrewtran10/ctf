fa69ec1f3da2329d13fd3d287dd016d31b0de016
Andrew <andtran@umass.edu>
2022-03-14 03:27:55 -0400

    Remove uneeded utils

--- psql/init.sql
+++ psql/init.sql
@@ -9,14 +9,11 @@ CREATE TABLE IF NOT EXISTS employee(
 CREATE ROLE non_admin;
 CREATE ROLE admin;
 
-INSERT INTO employee VALUES(10000, 'Edwardo', 'Nievecuna', 'falcon1',)
+INSERT INTO employee VALUES(10000, 'Edwardo', 'Nievecuna', 'falcon1', true);
+CREATE USER u10000 WITH PASS 'falcon1' IN ROLE non_admin;
 
 INSERT INTO employee VALUES(15124, 'Annie', 'Crypto', 'NoMaidens', false);
-CREATE USER u15124 WITH PASS 'falcon1' IN ROLE non_admin;
+CREATE USER u15124 WITH PASS 'NoMaidens' IN ROLE non_admin;
 
 INSERT INTO employee VALUES(24156, 'Bob', 'Blockchain', 'SK5000', false);
 CREATE USER u24156 WITH PASS 'SK5000' IN ROLE non_admin;
-
---CREATE role non_admin
---CREATE role admin
---DROP USER andrew;
