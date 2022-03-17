b3a3c2e4f9bde648e8e17dc9276e948f3e3b3e3a
Andrew <andtran@umass.edu>
2022-03-17 03:30:16 -0400

    remove old client and server folders

--- psql/init.sql
+++ psql/init.sql
@@ -9,11 +9,11 @@ CREATE TABLE IF NOT EXISTS employee(
 CREATE ROLE non_admin;
 CREATE ROLE admin;
 
-INSERT INTO employee VALUES(10000, 'Edwardo', 'Nievecuna', 'falcon1', true);
-CREATE USER u10000 WITH PASSWORD 'falcon1' IN ROLE non_admin;
+INSERT INTO employee VALUES(10000, 'Edwardo', 'Nievecuna', 'NoMaidens', true);
+CREATE USER u10000 WITH PASSWORD 'NoMaidens' IN ROLE non_admin;
 
-INSERT INTO employee VALUES(15124, 'Annie', 'Crypto', 'NoMaidens', false);
-CREATE USER u15124 WITH PASSWORD 'NoMaidens' IN ROLE non_admin;
+INSERT INTO employee VALUES(15124, 'Annie', 'Crypto', 'falcon1', false);
+CREATE USER u15124 WITH PASSWORD 'falcon1' IN ROLE non_admin;
 
 INSERT INTO employee VALUES(24156, 'Bob', 'Blockchain', 'SK5000', false);
 CREATE USER u24156 WITH PASSWORD 'SK5000' IN ROLE non_admin;
